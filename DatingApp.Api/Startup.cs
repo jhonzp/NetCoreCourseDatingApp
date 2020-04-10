b23c43712fb4989bf697b136faaaa1aaafb51291
Jhon Zuñiga <zuni230669>
2020-04-09 20:13:36 -0500

    Angular app call dating.api end point.

--- DatingApp.Api/Startup.cs
+++ DatingApp.Api/Startup.cs
@@ -28,7 +28,8 @@ namespace DatingApp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));                      
-            services.AddControllers();            
+            services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,8 @@ namespace DatingApp.Api
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
