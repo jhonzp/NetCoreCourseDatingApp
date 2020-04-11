e6bba50ab5de840d2b40fba9f4f29271e5076338
Jhon Zuñiga <zuni230669>
2020-04-11 17:32:37 -0500

    Add home component and register component, and example to pass data from parent component to child compoment.

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,17 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
-
+export class HomeComponent implements OnInit {
+  registerMode = false;
   values: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getvalues();
+    this.values = this.getvalues();
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
   }
 
   getvalues() {
@@ -24,4 +28,5 @@ export class ValueComponent implements OnInit {
       }
     );
   }
+
 }
