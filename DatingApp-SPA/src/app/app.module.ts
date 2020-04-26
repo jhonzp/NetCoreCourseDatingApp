bf57d16f9694bc4b75b8b88807e71e1ab06d08b5
Jhon Zuñiga <zuni230669>
2020-04-26 13:12:30 -0500

    it use ngxGallery to show photos

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -26,6 +27,12 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 export function tokenGetter(){
    return localStorage.getItem('token');
 }
+export class CustomHammerConfig extends HammerGestureConfig {
+   overrides = {
+     pinch: { enable: false },
+     rotate: { enable: false }
+   };
+ }
 
 
 @NgModule({
@@ -47,6 +54,7 @@ export function tokenGetter(){
       TabsModule.forRoot(),
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
       JwtModule.forRoot({
          config: {
             tokenGetter: tokenGetter,
@@ -59,7 +67,8 @@ export function tokenGetter(){
       AuthService,
       ErrorInterceptorProvider,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
    bootstrap: [
       AppComponent
